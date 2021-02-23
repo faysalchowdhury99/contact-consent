7173b9da1465e36b06f7c1cad4965afca764b5a9
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-02-24 02:31:31 +0600

    Added All Files

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,22 @@
+// Sticky Nav
+const navbar = document.querySelector('#header-section nav');
+window.onscroll = function () {
+    stickyNav();
+};
+function stickyNav() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        navbar.classList.add('sticky-nav');
+    } else {
+        navbar.classList.remove('sticky-nav');
+    }
+}
+
+// Mobile Menu Icon Animation
+const navbarToggler = document.querySelector('.navbar-toggler');
+navbarToggler.addEventListener('click', () => {
+    navbarToggler.classList.toggle('toggle');
+});
+
 // Selected Radio Button Check
 const btnContainer = document.querySelector('.solution-radio-box');
 const btns = btnContainer.getElementsByClassName('solution-radio');
